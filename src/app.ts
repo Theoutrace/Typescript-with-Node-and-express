62cd4d150b247d73d9b78c374d58aa992419465e
Theoutrace <prakashkumar.imw@gmail.com>
2023-02-14 21:38:02 +0530

    typescript with node and express

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import todosRoutes from "./routes/todos";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(todosRoutes);
+app.use(bodyParser.json());
+
+app.listen(3000);
